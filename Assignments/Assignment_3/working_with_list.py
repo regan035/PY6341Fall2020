0683d95aa9f7912ce2afc11307bdb2dce58e8483
regan035 <regan035@gmail.comgit config --global user.name regan035git statusgit config --global user.email regan035@gmail.com>
2020-09-13 23:24:48 -0500

    upload assignment_3

--- Assignments/Assignment_3/working_with_list.py
+++ Assignments/Assignment_3/working_with_list.py
@@ -6,28 +6,33 @@
 # Assignment #3
 # Assignment Title: Working with lists
 
-city_list = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix','Philadelphia', 'San Anthonio', 'San Diego', 'Dallas', 'San Jose']
+city_list = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix',
+             'Philadelphia', 'San Anthonio', 'San Diego', 'Dallas', 'San Jose']
 
-population_list = ['8,336,817', '3,979,576', '2,693,976', '2,320,268','1,680,992', '1,680,992', '1,547,253', '1,423,851', '1,343,573', '1,021,795']
+population_list = [8336817, 3979576, 2693976, 2320268,
+                   1680992, 1680992, 1547253, 1423851, 1343573, 1021795]
 
 #1.Print the entire city list and population list in two separate lines.
 print(city_list)
-print("\n")
 print("\n")
 print(population_list)
 print("\n")
 
 #2. Print the 3rd item followed by its population
-print("The 3rd city in the list is "+city_list[2]+".")
-print("The population is "+population_list[2]+".")
+third_pop = population_list[2]
+print(
+    f"The 3rd city in the list is {city_list[2]}. The population is {third_pop:,}.")
 print("\n")
+
 #3. Print the last item in the city list followed by its population.
-print("The last city in the list is "+city_list[-1]+".")
-print("The population is "+population_list[-1]+".")
+last_pop = population_list[-1]
+print(
+    f"The last city in the list is {city_list[-1]}. The population is {last_pop:,}.")
 print("\n")
+
 #4. Add a new city in 6th item position of city list and the new cities population on the population listexactly on 6th position. (now both lists will have 11 items)
 city_list.insert(5, 'Austin')
-population_list.insert(5, '978,908')
+population_list.insert(5, 978908)
 
 #5. Print the list of all cities
 print(city_list)
@@ -42,19 +47,14 @@ population_list.pop(2)
 #8. Print the both list in two separate line.
 print(city_list)
 print("\n")
-print("\n")
 print(population_list)
 print("\n")
 
-
 #9. Print both sorted list in a separate line
-print(sorted(city_list))
+print((sorted(city_list)), (sorted(population_list)))
 print("\n")
-print(sorted(population_list))
-print("\n")
-
 
 #10. Double the population of the 2nd item in the population list
-second_item = population_list[1]
-new_pop = (int(second_item.replace(',', '')))*2
+
+new_pop = population_list[1]*2
 print(f"The double of 2nd item in population list is {new_pop:,}.")
