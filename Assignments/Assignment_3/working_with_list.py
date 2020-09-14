accf86bdb852dcb52e148b81084caa826d9c9698
regan035 <regan035@gmail.comgit config --global user.name regan035git statusgit config --global user.email regan035@gmail.com>
2020-09-13 22:36:53 -0500

    adjust separate lines

--- Assignments/Assignment_3/working_with_list.py
+++ Assignments/Assignment_3/working_with_list.py
@@ -13,27 +13,28 @@ population_list = ['8,336,817', '3,979,576', '2,693,976', '2,320,268','1,680,992
 #1.Print the entire city list and population list in two separate lines.
 print(city_list)
 print("\n")
+print("\n")
 print(population_list)
 print("\n")
 
 #2. Print the 3rd item followed by its population
 print("The 3rd city in the list is "+city_list[2]+".")
 print("The population is "+population_list[2]+".")
-
+print("\n")
 #3. Print the last item in the city list followed by its population.
 print("The last city in the list is "+city_list[-1]+".")
 print("The population is "+population_list[-1]+".")
-
+print("\n")
 #4. Add a new city in 6th item position of city list and the new cities population on the population listexactly on 6th position. (now both lists will have 11 items)
 city_list.insert(5, 'Austin')
 population_list.insert(5, '978,908')
 
 #5. Print the list of all cities
 print(city_list)
-
+print("\n")
 #6. Print the list of all population
 print(population_list)
-
+print("\n")
 #7. Remove the 3rd item from city list and the corresponding population item from population list.
 city_list.pop(2)
 population_list.pop(2)
@@ -41,12 +42,14 @@ population_list.pop(2)
 #8. Print the both list in two separate line.
 print(city_list)
 print("\n")
+print("\n")
 print(population_list)
 print("\n")
 
 
 #9. Print both sorted list in a separate line
 print(sorted(city_list))
+print("\n")
 print(sorted(population_list))
 print("\n")
 
