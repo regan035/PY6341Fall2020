b678ec7cee058125840509db4170ca43189c168a
regan035 <regan035@gmail.comgit config --global user.name regan035git statusgit config --global user.email regan035@gmail.com>
2020-10-15 17:27:55 -0500

    upload assignment_7

--- Assignments/Assignment_7/working_with_input_and_while.py
+++ Assignments/Assignment_7/working_with_input_and_while.py
@@ -5,18 +5,19 @@
 
 # Assignment #6
 # Assignment Title :Workint with input and while
+
 print('Please input the name and monthly income for each person.')
 print('Please enter ‘quit’ in the name field to stop the user input.')
 print()
 name_salary = {}
-formatted_marks = '{:.2f}'
+formatted_marks = '${:.2f}'
+
 active = True
 while active:
   name_input = input('Please type in the name: ')
   if name_input == 'quit':
       break
       active = False
-
   salary_input = input('Please type in the salary: ')
   salary = float(salary_input)
   name_salary[name_input.title()] = salary
@@ -34,8 +35,8 @@ print('='*50)
 
 max_salary = max(name_salary, key=name_salary.get)
 print(
-    f'{max_salary} makes the hightest monthly income ${formatted_marks.format(name_salary[max_salary])}')
+    f'{max_salary} makes the hightest monthly income {formatted_marks.format(name_salary[max_salary])}')
 min_salary = min(name_salary, key=name_salary.get)
 print(
-    f'{min_salary} makes the lowest monlthly income ${formatted_marks.format(name_salary[min_salary])}')
+    f'{min_salary} makes the lowest monlthly income {formatted_marks.format(name_salary[min_salary])}')
 print('-'*50)
