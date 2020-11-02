0e0178f215ec0d87a102ec5eaef6e52d734822f3
regan035 <regan035@gmail.comgit config --global user.name regan035git statusgit config --global user.email regan035@gmail.com>
2020-11-01 20:51:33 -0600

    upload assignment_9

--- Assignments/Assignment_9/working_with_classes.py
+++ Assignments/Assignment_9/working_with_classes.py
@@ -19,6 +19,7 @@ class Trapezoid(Quadrilaterals):
     def __init__(self,name,base,height):
         super().__init__(name, base, height)
 
+    # Override CalculationArea in parent class
     def CalculateArea(self):
         return((self.base+self.height)/2)*self.height
 
@@ -26,18 +27,22 @@ class Rectangle(Quadrilaterals):
     def __init__(self, name, base, height):
         super().__init__(name, base, height)
 
+    # Override CalculationArea in parent class
     def CalculateArea(self):
         return self.base * self.height
 
+#Create new instances for Trapezoid and Rectangle classes
 t1 = Trapezoid('Trapezoid1',20,40)
 t2 = Trapezoid('Trapezoid2',2,4)
 
 r1 = Rectangle('Rectangle1',20,40)
 r2 = Rectangle('Rectangle2',2,4)
-r3 = Rectangle('Rectangle3',5,10)
+r3 = Rectangle('Rectangle3',5.5,9.5)
 
-print(f'The area of {t1.name} is {t1.CalculateArea()}')
-print(f'The area of {t2.name} is {t2.CalculateArea()}')
-print(f'The area of {r1.name} is {r1.CalculateArea()}')
-print(f'The area of {r2.name} is {r2.CalculateArea()}')
-print(f'The area of {r3.name} is {r3.CalculateArea()}')
+print()
+print(f'The area of {t1.name} is {float(t1.CalculateArea())}')
+print(f'The area of {t2.name} is {float(t2.CalculateArea())}')
+print(f'The area of {r1.name} is {float(r1.CalculateArea())}')
+print(f'The area of {r2.name} is {float(r2.CalculateArea())}')
+print(f'The area of {r3.name} is {float(r3.CalculateArea())}')
+print()
