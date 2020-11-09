a8ce7e5c701cdf769fe7d144823cc3cc4ab392f2
regan035 <regan035@gmail.comgit config --global user.name regan035git statusgit config --global user.email regan035@gmail.com>
2020-11-08 22:41:42 -0600

    add ship class

--- InClassWork/CH12 A ship that fires bullets/alien_invasion.py
+++ InClassWork/CH12 A ship that fires bullets/alien_invasion.py
@@ -1,14 +1,20 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlianInvasion:
     """Overall class to manage game assets and behavior."""
     def __init__(self):
         """Initial the game, and create game resources."""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-    
+        self.ship = Ship(self)
+        # Set the blackground color.
+        self.bg_color = (230, 230, 230)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -16,11 +22,13 @@ class AlianInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-        
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             # Make the most recently draw screen visible.
             pygame.display.flip()
     
-    if __name__=='__main__':
-        #Make a game instance, and run the game.
-        ai = AlianInvasion()
-        ai.run_game()
+if __name__=='__main__':
+    #Make a game instance, and run the game.
+    ai = AlianInvasion()
+    ai.run_game()
