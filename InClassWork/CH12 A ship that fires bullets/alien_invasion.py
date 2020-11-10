932fb3c9bfab74f2190a8598a465314699c52772
regan035 <regan035@gmail.comgit config --global user.name regan035git statusgit config --global user.email regan035@gmail.com>
2020-11-10 13:44:17 -0600

    finish ch12, need trouble shooting ship.png

--- InClassWork/CH12 A ship that fires bullets/alien_invasion.py
+++ InClassWork/CH12 A ship that fires bullets/alien_invasion.py
@@ -32,7 +32,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
     
     def _check_events(self):
@@ -70,8 +70,20 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullet_allowd:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """update postion of bullet and get rid of old bulltes"""
+        #Update bullet positions
+        self.bullets.update()
+        # Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+            print(len(self.bullets))
+
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
